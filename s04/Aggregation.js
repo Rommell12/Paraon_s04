d74ad9fa7dc4460b098e25bc52be2ee2d7ff9b13
Rommell12 <91557792+Rommell12@users.noreply.github.com>
2022-09-22 16:41:29 +0800

    Aggregation_added_activity
    Aggregation_added_activity

---
+++ s04/Aggregation.js
@@ -0,0 +1,273 @@
+
+    Employee
+[   
+    /* 1 */
+    {
+        "_id" : ObjectId("632c03caf7319ecd812c2ad3"),
+        "id" : "863473",
+        "name" : "Mangkanor Lacson",
+        "age" : 15,
+        "jobRole" : "Manager",
+        "salary" : "50,000"
+    },
+
+    /* 2 */
+    {
+        "_id" : ObjectId("632c0492f7319ecd812c2afa"),
+        "id" : "123445",
+        "name" : "saitama senpai",
+        "age" : 22,
+        "jobRole" : "Associate",
+        "salary" : "30,000"
+    },
+
+    /* 3 */
+    {
+        "_id" : ObjectId("632c0501f7319ecd812c2b0e"),
+        "id" : "334566",
+        "name" : "Lebron James",
+        "age" : 18,
+        "jobRole" : "Cashier",
+        "salary" : "5,000"
+    },
+
+    /* 4 */
+    {
+        "_id" : ObjectId("632c05ccf7319ecd812c2b34"),
+        "id" : "245345",
+        "name" : "San Goku",
+        "age" : 75,
+        "jobRole" : "Senior Cashier",
+        "salary" : "5,000"
+    },
+
+    /* 5 */
+    {
+        "_id" : ObjectId("632c061bf7319ecd812c2b4a"),
+        "id" : "445364",
+        "name" : "Rommel Paraon pogi",
+        "age" : 22,
+        "jobRole" : "Main manager",
+        "salary" : "100,000"
+    }
+
+],
+    Product 
+    [
+    /* 1 */
+    {
+        "_id" : ObjectId("632c079bf7319ecd812c2b83"),
+        "id" : "RK12345667",
+        "name" : "Phillipines Rice",
+        "price" : "5,000",
+        "quantity" : "100,000",
+        "category" : [ 
+            "batangas Rice", 
+            "NFA"
+        ]
+    },
+
+    /* 2 */
+    {
+        "_id" : ObjectId("632c0838f7319ecd812c2ba2"),
+        "id" : "RK123456",
+        "name" : "Italy RICE",
+        "price" : "10,000",
+        "quantity" : "200,000",
+        "category" : [ 
+            "Roman Rice", 
+            "Poor Rice"
+        ]
+    },
+
+    /* 3 */
+    {
+        "_id" : ObjectId("632c08c9f7319ecd812c2bba"),
+        "id" : "RK1234567",
+        "name" : "Indian Rice",
+        "price" : "7,000",
+        "quantity" : "300,000",
+        "category" : [ 
+            "Long Rice", 
+            "Small Rice"
+        ]
+    },
+
+    /* 4 */
+    {
+        "_id" : ObjectId("632c0a2df7319ecd812c2be6"),
+        "id" : "RK12345678",
+        "name" : "Japan Rice",
+        "price" : "8,000",
+        "quantity" : "400,000",
+        "category" : [ 
+            "Brown Rice", 
+            "sticky rice"
+        ]
+    },
+
+    /* 5 */
+    {
+        "_id" : ObjectId("632c0ad4f7319ecd812c2bfd"),
+        "id" : "RK123456789",
+        "name" : "France Rice",
+        "price" : "10,000",
+        "quantity" : "600,000",
+        "category" : [ 
+            "Vegetables Rice", 
+            "Meat Rice", 
+            "Original Rice"
+        ]
+    },
+
+    /* 6 */
+    {
+        "_id" : ObjectId("632c0bcbf7319ecd812c2c1f"),
+        "id" : "Rk123456",
+        "name" : "China Rice",
+        "price" : "11,000",
+        "quantity" : "300,000",
+        "category" : [ 
+            "Plain Rice", 
+            "Hot Chilli Rice", 
+            "Organic Rice"
+        ]
+    },
+
+    /* 7 */
+    {
+        "_id" : ObjectId("632c0c27f7319ecd812c2c30"),
+        "id" : "LS0009100",
+        "name" : "American Rice",
+        "price" : "23,000",
+        "quantity" : "100,000",
+        "category" : [ 
+            "Chicken Rice", 
+            "Bamboo Rice", 
+            "Healthty Rice"
+        ]
+    },
+
+    /* 8 */
+    {
+        "_id" : ObjectId("632c0d48f7319ecd812c2c54"),
+        "id" : "LS0009100",
+        "name" : "Mexico Rice",
+        "price" : "33,000",
+        "quantity" : "200,000",
+        "category" : [ 
+            "sticky Rice", 
+            "Smooth Rice", 
+            "organic"
+        ]
+    }
+],
+    payment
+    [
+    /* 1 */
+    {
+        "_id" : ObjectId("632c0e94f7319ecd812c2c8e"),
+        "id" : "O023421005",
+        "grossAmount" : "2000",
+        "discounts" : "5%",
+        "netAmount" : "66.55",
+        "date/time" : "2018-02-3 8:30PM"
+    },
+
+    /* 2 */
+    {
+        "_id" : ObjectId("632c0f0bf7319ecd812c2ca5"),
+        "id" : "OO2021006",
+        "grossAmount" : "23.75",
+        "discounts" : "10%",
+        "netAmount" : "12.32",
+        "date/time" : "2021-03-26 9:30AM"
+    },
+
+    /* 3 */
+    {
+        "_id" : ObjectId("632c100bf7319ecd812c2cc9"),
+        "id" : "0023231241",
+        "grossAmount" : "534.123",
+        "discounts" : "20%",
+        "netAmount" : "244",
+        "date/time" : "2021-05-07 11:30AM"
+    },
+
+    /* 4 */
+    {
+        "_id" : ObjectId("632c101af7319ecd812c2ccc"),
+        "id" : "OO12345667",
+        "grossAmount" : "22",
+        "discounts" : "5%",
+        "netAmount" : "25",
+        "date/time" : "2021-09-09 12:30PM"
+    },
+
+    /* 5 */
+    {
+        "_id" : ObjectId("632c1028f7319ecd812c2ccf"),
+        "id" : "OO2021009",
+        "grossAmount" : "69.69",
+        "discounts" : "7%",
+        "netAmount" : "99",
+        "date/time" : "2021-01-01 3:30PM"
+    },
+
+    /* 6 */
+    {
+        "_id" : ObjectId("632c103af7319ecd812c2cd4"),
+        "id" : "002312021010",
+        "grossAmount" : "44.5",
+        "discounts" : "2%",
+        "netAmount" : "14",
+        "date/time" : "2015-03-03 5:00AM"
+    }
+    ],
+    Promo
+    [
+    /* 1 */
+    {
+        "_id" : ObjectId("632c11d0f7319ecd812c2d2d"),
+        "id" : "SALE23",
+        "name" : "Sales Rice",
+        "period" : 7,
+        "Wholesales" : ", 50, 12"
+    },
+
+    /* 2 */
+    {
+        "_id" : ObjectId("632c11f5f7319ecd812c2d3c"),
+        "id" : "SALE02",
+        "name" : "christmass Sale",
+        "period" : 2,
+        "WholeSale" : "120, 200"
+    },
+
+    /* 3 */
+    {
+        "_id" : ObjectId("632c1200f7319ecd812c2d43"),
+        "id" : "SALE123",
+        "name" : "Meat Promo",
+        "period" : 3,
+        "WholeSales" : "101, 205"
+    },
+
+    /* 4 */
+    {
+        "_id" : ObjectId("632c120df7319ecd812c2d4f"),
+        "id" : "SALE234",
+        "name" : "New Year Sale",
+        "period" : 7,
+        "WholeSales" : "10, 69, 35, 100, "
+    },
+
+    /* 5 */
+    {
+        "_id" : ObjectId("632c122df7319ecd812c2d5b"),
+        "id" : "SALE234",
+        "name" : "Scholing Sale",
+        "period" : 7,
+        "WholeSales" : "10, 69, 35, 100, "
+    }
+]
